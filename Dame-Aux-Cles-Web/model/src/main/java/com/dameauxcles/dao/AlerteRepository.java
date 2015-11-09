8633871357a27c8d8bf4637aa8e05894f3d0b324
Adrien74 <adrien.detraz@gmail.com>
2015-11-09 17:54:23 +0100

    Alerte repository modif

--- Dame-Aux-Cles-Web/model/src/main/java/com/dameauxcles/dao/AlerteRepository.java
+++ Dame-Aux-Cles-Web/model/src/main/java/com/dameauxcles/dao/AlerteRepository.java
@@ -1,10 +1,12 @@
 package com.dameauxcles.dao;
 
+import java.util.ArrayList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dameauxcles.model.Alert;
 
 public interface AlerteRepository extends JpaRepository<Alert, Long>{
-
-	
+	ArrayList<Alert> findByIdPersonne_loginPersonne(String loginPersonne);
+	 
 }
