d838b4b02dd48af290fdae33f214ae71d4009e77
caballoj <julien.caballol@hotmail.fr>
2015-11-30 09:16:09 +0100

    Ajout d'un bouton de création d'alertes

--- Dame-Aux-Cles-Web/ui/src/main/resources/static/js/application.js
+++ Dame-Aux-Cles-Web/ui/src/main/resources/static/js/application.js
@@ -41,4 +41,26 @@ application.directive('file', function(){
     };
 });
 
+(function(){
+
+    'use strict';
+
+    function ngRedirectTo($window) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attributes) {
+                element.bind('click', function (event) {
+                    //assign ng-Redirect-To attribute value to location
+                    $window.location.href = attributes.ngRedirectTo;
+                });
+            }
+        };
+    }
+    application.directive('ngRedirectTo', ngRedirectTo);
+    //inject $window service for redirection
+    redirectTo.$inject = ['$window'];
+}());
+
+
+
 
